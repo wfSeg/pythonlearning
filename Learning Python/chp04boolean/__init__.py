90a21e8fd6c7112add251e4db30837705520c545
Isador <Isador@Isador-PC.supermicro.com>
2017-08-01 16:04:59 -0700

    commit 1 thing

---
+++ Learning Python/chp04boolean/__init__.py
@@ -0,0 +1,17 @@
+#look at comment and set it to True or False
+
+#170 < 300
+bool_a = True
+
+#10==(2*5)
+bool_b = True
+
+#21<=21
+bool_c = True
+
+#-25>=-21
+bool_d = False
+
+#100!=(99+1)
+bool_e = False
+
