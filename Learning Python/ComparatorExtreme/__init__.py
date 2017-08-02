a72adfe823f7543c4789c05b33230e96943e6402
Isador <Isador@Isador-PC.supermicro.com>
2017-08-02 09:41:31 -0700

    day three, i am still alive.

---
+++ Learning Python/ComparatorExtreme/__init__.py
@@ -0,0 +1,16 @@
+#learning python is a hands-on thing hmmmmmm
+
+#(30 - 10) > 25
+sample_one = False
+
+#(20+7) == 2**10
+sample_two = False
+
+#1**4 <= -2
+sample_three = False
+
+#20*4 >= -20
+sample_four = True
+
+#81 != 9**2
+sample_five = False
---
+++ Learning Python/chp04LuckyOne/__init__.py
@@ -0,0 +1,14 @@
+#Luckyone program
+
+answer = raw_input("Enter a, b, c, or d and see your luck: ")
+
+if answer=='a':
+    print "You will meet a beautiful stranger!"
+elif answer=='b':
+    print "You will meet a fateful encouter, beware..."
+elif answer=='c':
+    print "You will get a reward from your mom!"
+elif answer=='d':
+    print "Today your life will completely change!"
+else:
+    print "You will be unlucky for the rest of your life!"
