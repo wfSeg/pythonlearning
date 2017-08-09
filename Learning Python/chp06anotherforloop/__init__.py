05bc3196d677596ee2fa97b5bd133530788d304d
Isador <Isador@172.16.101.231>
2017-08-09 11:51:19 -0700

    8/9 did this yesterday, forgot to commit it

---
+++ Learning Python/chp06anotherforloop/__init__.py
@@ -0,0 +1,11 @@
+#sample
+
+chant = raw_input("Who do you cheer for? ")
+
+for character in chant:
+    chanting="Gimme a "+character+"!"
+    print chanting
+    print character+"!"
+    
+print "What does that spell?"
+print chant+"!"
---
+++ Learning Python/chp06forloop/__init__.py
@@ -0,0 +1,17 @@
+#sample "For" loop
+
+this_list = [45,14,65,42,34]
+
+#for loop
+
+for content in this_list:
+    print content
+    
+print "It's the end."
+this_list.append(24)
+
+#using append to add to list
+for content in this_list:
+    print content
+    
+print "Wow 24 got added to the list"
