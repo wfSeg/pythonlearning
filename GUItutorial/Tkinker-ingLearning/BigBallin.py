0e02a683f5b6d911cdf6dffcb15e58a529aec28b
Isador <Isador@172.16.101.231>
2017-09-21 17:23:17 -0700

    need entry - and buttons to pop up another canvas menu

--- GUItutorial/Tkinker-ingLearning/BigBallin.py
+++ GUItutorial/Tkinker-ingLearning/BigBallin.py
@@ -21,69 +21,95 @@ def center(win):
     
 #Canvas
 baller=Tk()
-canvas = Canvas(baller)
+canvas = Canvas(baller, width=200, height=200)
 canvas.pack()
 baller.title("Big Baller 3.0")
 
 from PIL import ImageTk, Image
 #Logo
-path = "BigBallerBrand.jpg"
+path = "S:\images\BigBallerBrand.jpg"
 img = ImageTk.PhotoImage(Image.open(path))
-panel = baller.Label(window, image = img)
+panel = Tkinter.Label(baller, image = img)
 panel.pack(side = "bottom", fill = "both", expand = "yes")
 
 #Entry Field 1 IPMI IP Address
 entry1label = Label(baller, text="IPMI IP Address ")
 entry1label.pack(side = LEFT)
-entry1label_window = canvas.create_window(20, 10, anchor=NW, window=entry1label)
+entry1label_window = canvas.create_window(-40, 20, anchor=NW, window=entry1label)
 entry1entry = Entry(baller, bd = 5)
 entry1entry.pack(side = RIGHT)
-entry1entry_window = canvas.create_window(20, 30, anchor=NW, window=entry1entry)
+entry1entry.focus_set()
+entry1entry_window = canvas.create_window(60, 20, anchor=NW, window=entry1entry)
 print (entry1label.winfo_geometry())
 print (entry1entry.winfo_geometry())
+
+""" need figure out Entry
+def e1():
+    print entry1entry.get()
+#Entry 1 button check
+e1b = Button(text = "check", command = e1)
+e1b.configure(width = 0,
+                  activebackground = "Yellow",
+                  relief = GROOVE)
+e1b_window = canvas.create_window(100, 20, anchor=NW, window=e1b)
+e1b.update()
+print (e1b.winfo_geometry())
+"""
 
 #Entry Field 2 IPMI User Name
 entry2label = Label(baller, text="IPMI Username")
 entry2label.pack(side = LEFT)
-entry2label_window = canvas.create_window(20, 50, anchor=NW, window=entry2label)
+entry2label_window = canvas.create_window(-40, 60, anchor=NW, window=entry2label)
 entry2entry = Entry(baller, bd = 5)
 entry2entry.pack(side = RIGHT)
-entry2entry_window = canvas.create_window(20, 70, anchor=NW, window=entry2entry)
+entry2entry_window = canvas.create_window(60, 60, anchor=NW, window=entry2entry)
 print (entry2label.winfo_geometry())
 print (entry2entry.winfo_geometry())
 
 #Entry Field 3 IPMI User Name
 entry3label = Label(baller, text="IPMI Password")
 entry3label.pack(side = LEFT)
-entry3label_window = canvas.create_window(20, 90, anchor=NW, window=entry3label)
+entry3label_window = canvas.create_window(-40, 100, anchor=NW, window=entry3label)
 entry3entry = Entry(baller, bd = 5)
 entry3entry.pack(side = RIGHT)
-entry3entry_window = canvas.create_window(20, 110, anchor=NW, window=entry3entry)
+entry3entry_window = canvas.create_window(60, 100, anchor=NW, window=entry3entry)
 print (entry3label.winfo_geometry())
 print (entry3entry.winfo_geometry())
 
-button1 = Button(text = "BIOS Scripts")
+#"i got no scripts" box
+def Placeholder():
+    tkMessageBox.showinfo("Ballin'", "Sorry, We're All Sold Out!\nCheck back again later")
+   
+button1 = Button(text = "BIOS Scripts", command = Placeholder)
 button1.configure(width = 0,
                   activebackground = "Yellow",
                   relief = GROOVE)
-button1_window = canvas.create_window(20, 150, anchor=NW, window=button1)
+button1_window = canvas.create_window(-60, 140, anchor=NW, window=button1)
 button1.update()
 print (button1.winfo_geometry())
 
-button2 = Button(text = "IPMI Scripts")
+button2 = Button(text = "IPMI Scripts", command = Placeholder)
 button2.configure(width = 0,
                   activebackground = "Yellow",
                   relief = GROOVE)
-button2_window = canvas.create_window(20, 190, anchor=NW, window=button2)
+button2_window = canvas.create_window(20, 140, anchor=NW, window=button2)
 button2.update()
 print (button2.winfo_geometry())
 
-button3 = Button(text = "Other Scripts")
+button3 = Button(text = "Other Scripts", command = Placeholder)
 button3.configure(width = 0,
                   activebackground = "Yellow",
                   relief = GROOVE)
-button3_window = canvas.create_window(20, 230, anchor=NW, window=button3)
+button3_window = canvas.create_window(100, 140, anchor=NW, window=button3)
 button3.update()
+print (button3.winfo_geometry())
+
+button4 = Button(text = "Order ZO2 Prime Remix", command = Placeholder)
+button4.configure(width = 0,
+                  activebackground = "Yellow",
+                  relief = GROOVE)
+button4_window = canvas.create_window(-60, 180, anchor=NW, window=button4)
+button4.update()
 print (button3.winfo_geometry())
 
 center(baller)
---
+++ GUItutorial/Tkinker-ingLearning/Entry-howdoesitwork.py
@@ -0,0 +1,29 @@
+'''
+Created on Sep 21, 2017
+
+@author: yeezy
+'''
+
+from Tkinter import *
+
+master = Tk()
+
+e = Entry(master, width=50)
+e.pack()
+
+text = e.get()
+def makeentry(parent, caption, width=None, **options):
+    Label(parent, text=caption).pack(side=LEFT)
+    entry = Entry(parent, **options)
+    if width:
+        entry.config(width=width)
+    entry.pack(side=LEFT)
+    return entry
+
+user = makeentry(parent, "User name:", 10)
+password = makeentry(parent, "Password:", 10, show="*")
+content = StringVar()
+entry = Entry(parent, text=caption, textvariable=content)
+
+text = content.get()
+content.set(text)
